7d34d78894c7c377330605b992db63d31e274866
Tomeu <tcampaner@yahoo.com>
2020-11-18 13:23:01 +0100

    verssió inicial

---
+++ app/config/db.const.php
@@ -0,0 +1,8 @@
+<?php
+    // Definim constants. Per exemple connexió a la BBDD
+    define("BBDD_USER", "");
+    define("BBDD_PASS", "");
+    define("BBDD_NAME", "");
+    define("BBDD_HOST", "localhost");
+    define("PATH_SERVER", "");
+?>
